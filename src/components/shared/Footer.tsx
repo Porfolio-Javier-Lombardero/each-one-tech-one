c0aec70d8198c5d052f19482d14789dad157fbed
Javier <javier.lombar2@gmail.com>
2026-05-11 20:30:19 +0200

    correcion icono facebook, añadimos favicon y corregimos title de la pagina

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -5,10 +5,10 @@ export const Footer = () => {
     
 <footer className="bg-primary text-white text-center py-3">
     <div className="container">
-        <p className="mb-0">© 2025 Each-one-Tech-one. All rights reserved.</p>
+        <p className="mb-0">© 2026 Each-one-Tech-one. All rights reserved.</p>
         <p className="mb-0">
             Follow us on 
-            <a href="https://facebook.com" className="text-white mx-2">Facebook</a> | 
+            <a href="https://facebook.com" className="text-white mx-2">Instagram</a> | 
             <a href="https://x.com/EachTechNews" className="text-white mx-2">X</a> | 
             <a href="https://www.instagram.com/eachonetechone/" className="text-white mx-2">Instagram</a>    </p>
     </div>
